d70dab93621ceff401e8f763669d032ed7704a90
Olena-Zhyzhko <asil198609@gmail.com>
2022-09-12 21:15:43 +0300

    render done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,12 @@
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
+import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 import user from 'path/to/user.json';
 import data from 'path/to/data.json';
 import friends from 'path/to/friends.json';
+import transactions from "path/to/transactions.json";
+
 
 
 export const App = () => {
@@ -20,6 +24,8 @@ export const App = () => {
       <Profile user={user} />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
+
 
       {/* <Statistics stats={data} /> */}
       {/* React homework template */}
--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,7 +1,26 @@
 import PropTypes from 'prop-types';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
 
 export const FriendList = ({ friends }) => {
     return <ul class="friend-list">
-        <!-- Довільна кіл-сть FriendListItem -->
+        {friends.map(({ id, avatar, name, isOnline }) => (
+                <FriendListItem
+                key={id}
+                avatar={avatar}
+                name={name}
+                isOnline={isOnline} />
+                )
+            )}
     </ul>
+}
+
+FriendList.propTypes = {
+    friends: PropTypes.arrayOf(
+        PropTypes.exact({
+            id: PropTypes.number.isRequired,
+            avatar: PropTypes.string.isRequired,
+            name: PropTypes.string.isRequired,
+            isOnline: PropTypes.bool.isRequired,
+        })
+    )
 }
